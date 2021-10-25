7cf17b8030f8172ff042d8b20c3e6346e3fb56ca
ekaterinamusatova <91495066+ekaterinamusatova@users.noreply.github.com>
2021-10-25 22:35:13 +0500

    ДЗ-4

---
+++ Ekaterina_Musatova_дз4/Ekaterina_Musatova_дз4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ekaterina_Musatova_дз4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = XY1(0, XY1(1, XY1(2, 3 + Math.Sqrt(5))));
+            Console.WriteLine(Math.Round(x,3));
+        }
+        static double XY1(double n1, double n2)
+        {
+            return n1 + (1/n2);
+        }
+    }
+}
