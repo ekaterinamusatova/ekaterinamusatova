467ebe284bd040c3422fa4b56987f7e63cdc5224
ekaterinamusatova <91495066+ekaterinamusatova@users.noreply.github.com>
2021-10-25 16:45:45 +0500

    ДЗ-1

---
+++ Екатерина_Мусатова.дз/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Екатерина_Мусатова.дз
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Анна Ахматова <<А мы?>>");
+            {
+                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                {
+                    Console.WriteLine("А мы?");
+                    Console.ReadKey();
+                    Console.WriteLine("Не так же ль мы");
+                    Console.ReadKey();
+                    Console.WriteLine("Сошлись на краткий миг для переклички?");
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("Анна Ахматова <<А мы?>>");
+                        Console.WriteLine("А мы?");
+                        Console.WriteLine("Не так же ль мы");
+                        Console.WriteLine("Сошлись на краткий миг для переклички?");
+                    }
+                }
+            }
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+        }
+    }
+}
