a13c6b9793bd0ae203f201f5e9f246c849f30463
ekaterinamusatova <91495066+ekaterinamusatova@users.noreply.github.com>
2021-10-27 22:59:50 +0500

    /Дз-3

---
+++ Задание6.2/Task62.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Задание6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+            static bool НазваниеМетода(double x, double y)
+            {
+                return ((x <= -2) && (y >= 1));
+            }
+            Console.WriteLine(НазваниеМетода(x, y));
+            Console.ReadKey();
+        }
+    }
+}
