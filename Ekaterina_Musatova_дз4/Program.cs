9c0366064a7f917318ee7dc564aedff49b6b8174
ekaterinamusatova <91495066+ekaterinamusatova@users.noreply.github.com>
2021-11-16 22:33:18 +0500

    task3

---
+++ Ekaterina_Musatova_дз4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ekaterina_Musatova_дз3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = XY1(0, XY1(3, XY1(2, 1, Math.Pow(x, 2) + 1), Math.Pow(x, 2) + 2), Math.Pow(x, 2) + 3);
+            Console.WriteLine;
+        }
+        static double XY1(double n1, double n2, double n3)
+        {
+            return n1 + (n2/n3);
+        }
+    }
+}
