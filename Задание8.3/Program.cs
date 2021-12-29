2434dc176d867e73b2203862fe8269c0d6b3242b
ekaterinamusatova <91495066+ekaterinamusatova@users.noreply.github.com>
2021-12-29 22:46:37 +0500

    /

---
+++ Задание8.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Задание8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = 0;
+            while ((m * m * m + m + 1) < n)
+                m++;
+            Console.WriteLine(m * m * m + m + 1);
+            Console.ReadKey();
+        }
+    }
+}
